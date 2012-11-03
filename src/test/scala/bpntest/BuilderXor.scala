5d3d9df775ae8f6d6139242e796fd5f522a59b36
Mérő László <laci37@gmail.com>
2012-11-03 22:09:01 +0100

    tested ForwardBuilder, updated TODO

---
+++ src/test/scala/bpntest/BuilderXor.scala
@@ -0,0 +1,29 @@
+package bpntest
+import bpn._
+import math._
+import mathext._
+class BuilderXor {
+
+  val n = {
+    val bdr = new ForwardBuilder
+    import bdr._
+    create input 2 named "in"
+    create sigmoid 2 named "hid"
+    create sigmoid 1 named "out"
+    connection from "in" to "hid"
+    connection from "hid" to "out"
+    make output "out"
+    build
+  }
+
+  val inputs = Seq(Seq(RVector(Array(0d, 0d))),
+    Seq(RVector(Array(0d, 1d))),
+    Seq(RVector(Array(1d, 0d))),
+    Seq(RVector(Array(1d, 1d))))
+  val targets = Seq(Seq(RVector(Array(0d))),
+    Seq(RVector(Array(1d))),
+    Seq(RVector(Array(1d))),
+    Seq(RVector(Array(0d))))
+  val t = new Teacher(n, (inputs, targets))
+
+}
