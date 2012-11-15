13879ef6fff623b0ae2ad9db941d8b88158b9639
Mérő László <laci37@gmail.com>
2012-11-15 20:17:47 +0100

    added syntactic sugar for random choice to mathext

--- src/main/scala/mathext/mathext.scala
+++ src/main/scala/mathext/mathext.scala
@@ -1,11 +1,22 @@
 
 package object mathext {
-  
+  val Random= scala.util.Random
   class RVectorSeqWrap(val vec:RVector) extends IndexedSeq[Double]{
     def apply(i:Int)=vec(0,i)
     def length=vec.cols
   }
+
   implicit def RVector2Seq(vec:RVector): IndexedSeq[Double]={
     new RVectorSeqWrap(vec)
   }
+
+  class DoubleExt(val inner:Double){ 
+    def ?[T](a:T):RandomChoice[T]=new RandomChoice[T](inner,a)
+  }
+
+  class RandomChoice[T](val chance:Double, val a:T){ 
+    def |(b:T):T= if(Random.nextDouble<chance) a else b
+  }
+
+  implicit def double2DoubleExt(x:Double):DoubleExt= new DoubleExt(x)
 }
