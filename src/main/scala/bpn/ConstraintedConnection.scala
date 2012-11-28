9be972be7568beae60dba2cdcf03cc84ddc26c6e
Mérő László <laci37@gmail.com>
2012-11-28 23:10:21 +0100

    made ConstraintedConnection more foolproof

--- src/main/scala/bpn/ConstraintedConnection.scala
+++ src/main/scala/bpn/ConstraintedConnection.scala
@@ -3,6 +3,7 @@ import collection.mutable.Set
 import breeze.linalg._
 class ConstraintedConnection(in: LayerOutput, out: LayerInput, gd: GradientDescent) extends Connection(in, out, gd) {
  var constraints=Set[ConstraintedConnection]()
+ protected var changedWeights=false 
  
  def addConstraint(that:ConstraintedConnection)={
    that.weights=this.weights
@@ -10,13 +11,22 @@ class ConstraintedConnection(in: LayerOutput, out: LayerInput, gd: GradientDesce
    that.constraints.add(this)
  }
  
- 
- 
+  
+
  override def learn()={
-   val gradientsum=
-     (for(c<-constraints) yield c.dEdw).fold(DenseMatrix.zeros[Double](in.size,out.size))((a, b) ⇒ (a + b))+dEdw
-   weights+=gd.getDelta(gradientsum*(1d/constraints.size+1))
+   if(!changedWeights){ 
+     val gradientsum=
+       (for(c<-constraints) yield c.dEdw).fold(DenseMatrix.zeros[Double](in.size,out.size))((a, b) ⇒ (a + b))+dEdw
+     val delta=gd.getDelta(gradientsum:/(constraints.size+1).toDouble)
+     for(c<-constraints) c.changeWeights(delta,this)
+     weights+=delta
+   }
    out.learn()
+ }
+
+ protected def changeWeights(delta:DenseMatrix[Double], caller:ConstraintedConnection)={ 
+   changedWeights=true
+   weights+=delta
  }
  
 }
