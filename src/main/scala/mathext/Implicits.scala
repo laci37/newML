1d4a5a7ab77b1bd18cf6a0aedafbf1583e40c0b8
Mérő László <laci37@gmail.com>
2012-12-03 20:56:16 +0100

    fixed SoftmaxTest, added roughly equals(=~) op to DoubleExt

--- src/main/scala/mathext/Implicits.scala
+++ src/main/scala/mathext/Implicits.scala
@@ -6,6 +6,10 @@ object Implicits {
  
   class DoubleExt(val inner:Double){ 
     def ?[T](a:T):RandomChoice[T]=new RandomChoice[T](inner,a)
+    /**
+     * roughly equals (max 0.01% difference)
+     */ 
+    def =~(that:Double):Boolean = if(inner>=0) that>0.9999*inner && that<1.001*inner else that<0.9999*inner && that>1.001*inner
   }
 
   class RandomChoice[T](val chance:Double, val a:T){ 
--- src/test/scala/bpntest/SoftmaxTest.scala
+++ src/test/scala/bpntest/SoftmaxTest.scala
@@ -3,6 +3,7 @@ import bpn._
 import breeze.linalg._
 import org.scalatest.FunSuite
 import math._
+import mathext.Implicits._
 
 class SoftmaxTest extends FunSuite { 
   class MockInput(out:LayerInput) extends Connection(new ConnectionTest.MockInput,out,null){ 
@@ -17,20 +18,20 @@ class SoftmaxTest extends FunSuite {
     val in= new MockInput(sm)
     in.setZ= DenseMatrix.create(3,2,Array(1d,0d,0d,1d,1d,-1d))
     sm.forward()
-    assert(sm.y(0,0)==E/(2+E),"y(0,0) wrong")
-    assert(sm.y(1,0)==1/(2+E),"y(1,0) wrong")
-    assert(sm.y(2,0)==1/(2+E),"y(2,0) wrong")
-    assert(sm.y(0,0)==E/(2*E+1/E),"y(0,1) wrong")
-    assert(sm.y(1,0)==E/(2*E+1/E),"y(1,1) wrong")
-    assert(sm.y(2,0)==1/(2*E*E+1),"y(2,1) wrong")
+    assert(sm.y(0,0) =~ E/(2+E),"y(0,0) wrong "+sm.y(0,0))
+    assert(sm.y(1,0) =~ 1/(2+E),"y(1,0) wrong "+sm.y(1,0))
+    assert(sm.y(2,0) =~ 1/(2+E),"y(2,0) wrong "+sm.y(2,0))
+    assert(sm.y(0,1) =~ E/(2*E+1/E),"y(0,1) wrong "+sm.y(0,1))
+    assert(sm.y(1,1) =~ E/(2*E+1/E),"y(1,1) wrong "+sm.y(1,1))
+    assert(sm.y(2,1) =~ 1/(2*E*E+1),"y(2,1) wrong "+sm.y(2,1))
     sm.targets=DenseMatrix.create(3,2,Array(0d,1d,0d,0d,1d,0d))
-    assert(sm.avgSumErr==(-log(1/(2+E))-log(E/2*E+(1/E)))/2, "wrong error")
+    assert(sm.avgSumErr =~ (-log(1/(2+E))-log(E/(2*E+(1/E))))/2, "wrong error "+sm.avgSumErr)
     sm.backward()
-    assert(sm.dEdz(0,0)==E/(2+E),"dEdz(0,0) wrong")
-    assert(sm.dEdz(1,0)==1/(2+E)-1,"dEdz(1,0) wrong")
-    assert(sm.dEdz(2,0)==1/(2+E),"dEdz(2,0) wrong")
-    assert(sm.dEdz(0,1)==E/(2*E+1/E),"dEdz(0,1) wrong")
-    assert(sm.dEdz(1,1)==E/(2*E+1/E)-1,"dEdz(1,1) wrong")
-    assert(sm.dEdz(2,1)==1/(2*E*E+1),"dEdz(2,1) wrong")
+    assert(sm.dEdz(0,0) =~ E/(2+E),"dEdz(0,0) wrong "+sm.dEdz(0,0))
+    assert(sm.dEdz(1,0) =~ 1/(2+E)-1,"dEdz(1,0) wrong "+sm.dEdz(1,0))
+    assert(sm.dEdz(2,0) =~ 1/(2+E),"dEdz(2,0) wrong "+sm.dEdz(2,0))
+    assert(sm.dEdz(0,1) =~ E/(2*E+1/E),"dEdz(0,1) wrong "+sm.dEdz(0,1))
+    assert(sm.dEdz(1,1) =~ E/(2*E+1/E)-1,"dEdz(1,1) wrong "+sm.dEdz(1,1))
+    assert(sm.dEdz(2,1) =~ 1/(2*E*E+1),"dEdz(2,1) wrong "+sm.dEdz(2,1))
   }
 }
